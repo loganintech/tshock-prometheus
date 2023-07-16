0cedcca6888764c253323f75fb10211694162f63
Logan Saso <logansaso+tech@gmail.com>
2023-07-16 11:43:31 -0700

    Added gague for each player based on when they're online

--- TShockPrometheus/Collectors/ConnectedPlayers.cs
+++ TShockPrometheus/Collectors/ConnectedPlayers.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Prometheus;
 using TerrariaApi.Server;
 using TShockAPI;
@@ -38,13 +39,32 @@ namespace TShockPrometheus.Collectors {
     }
     #endregion
 
+    private Dictionary<string, Gauge> playersToGague = new Dictionary<string, Gauge>();
+
     #region Hooks
     /// <summary>
     /// Called when a player joins the server
     /// </summary>
     /// <param name="args">event arguments passed by hook</param>
-    private void OnJoin (JoinEventArgs args) {
+    private void OnJoin (JoinEventArgs args)
+    {
       collector.Inc(1);
+
+      TSPlayer player = TShock.Players[args.Who];
+      if (player == null) {
+        return;
+      }
+
+      Gauge gauge = playersToGague[player.Name];
+      if (gauge != null)
+      {
+        gauge.Set(1);
+        return;
+      }
+      
+      Gauge newGauge = Metrics.CreateGauge("connected_player", "connected player", player.Name);
+      newGauge.Set(1);
+      playersToGague.Add(player.Name, newGauge);
     }
 
     /// <summary>
@@ -54,6 +74,11 @@ namespace TShockPrometheus.Collectors {
     private void OnLeave (LeaveEventArgs args) {
       if (TShock.Players[args.Who] == null) return;
       collector.Dec(1);
+      
+      TSPlayer player = TShock.Players[args.Who];
+      Gauge gauge = playersToGague[player.Name];
+
+      gauge?.Set(0);
     }
     #endregion
   }
